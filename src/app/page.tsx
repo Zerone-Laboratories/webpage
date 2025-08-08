c6e4490e54a95dd7506cf561231d03d5d2c4e87c
Kumuditha Ranasinghe <ranasinghe24125@gmail.com>
2025-08-08 20:11:08 +0530

    feat: implement hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <div>Hello Zerone</div>
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20">
+        <Hero />
+        {/* <Skills />
+        <Encryption />
+        <Projects /> */}
+      </div>
+    </main>
   );
 }
